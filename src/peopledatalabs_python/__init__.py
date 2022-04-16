9dfde3989e79b2896cafdc44c38e81caf8e77d1c
Yoske Apolloni <lupodellasleppa@gmail.com>
2022-04-16 19:42:11 +0200

    Bumps version to v0.2.0

--- src/peopledatalabs_python/__init__.py
+++ src/peopledatalabs_python/__init__.py
@@ -6,4 +6,4 @@ PeopleDataLabs Python Client.
 from .main import PDLPY
 
 
-__version__ = '0.1.1'
+__version__ = "0.2.0"
--- tests/client/test_client.py
+++ tests/client/test_client.py
@@ -20,7 +20,7 @@ def test_version():
     """
     Version check.
     """
-    assert __version__ == "0.1.1"
+    assert __version__ == "0.2.0"
 
 
 @pytest.mark.usefixtures("fake_api_key")
